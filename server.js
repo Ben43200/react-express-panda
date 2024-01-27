78fd597ce44481b62a81401d232a31afdb0c63e7
meteoben <meteoben@gmail.com>
2024-01-27 11:09:56 +0100

    dernier commit ça marche mais postmaster error sur  outlook

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const nodemailer = require('nodemailer')
+// const smtpTransport = require("nodemailer-smtp-transport");
 const app = express()
 
 const cors = require('cors');
@@ -16,103 +17,197 @@ const cors = require('cors');
 // const app = express()
 const port = process.env.PORT || 5000
 
-
+//mise en commentaire pour test
 const buildPath = path.join(__dirname, 'build')
 
 
 app.use(express.static(buildPath))
+
+//fin commentaire test
+
+
+
 app.use(express.json())
 app.use(cors())
 
+//ancien code panda
+
+// app.post('/pet', (req,res) => {
+//     const { hasPetted } = req.body;
+//     if(hasPetted === true) {
+//       res.json({message: "♡", image: "happypanda"});
+//     } else {
+//       res.json({message: "..Why did you stop? T.T", image: "sadpanda"});
+//     }
+//   })
 
 
-app.post('/pet', (req,res) => {
-    const { hasPetted } = req.body;
-    if(hasPetted === true) {
-      res.json({message: "♡", image: "happypanda"});
-    } else {
-      res.json({message: "..Why did you stop? T.T", image: "sadpanda"});
-    }
-  })
 
 
 
+// // gets the static files from the build folder
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(buildPath, 'index.html'))
+// })
 
 
-// gets the static files from the build folder
-app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'))
-})
-
-
-// Showing that the server is online and running and listening for changes
+// // Showing that the server is online and running and listening for changes
 app.listen(port, () => {
   console.log(`Server is online on port: ${port}`)
 })
 
+//Fin ancien code panda
 
-
-const transporter = nodemailer.createTransport("SMTP", {
-  host: "smtp-mail.outlook.com", // hostname
-  secureConnection: false, // TLS requires secureConnection to be false
-  port: 587, // port for secure SMTP
-  auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-  },
-  tls: {
-      ciphers:'SSLv3'
-  }
-});
-// *
-// const transporter = nodemailer.createTransport("SMTP",{
-//   host: "smtp-mail.outlook.com", //replace with your email provider
-//   port: 587,
+// const transporter = nodemailer.createTransport( {
+//   host: "smtp-mail.outlook.com", // hostname
+//   secureConnection: false, // TLS requires secureConnection to be false
+//   port: 587, // port for secure SMTP
 //   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD
+//       user: process.env.EMAIL,
+//       pass: process.env.PASSWORD
+//   }
+
+// });
+// // *
+// // const transporter = nodemailer.createTransport("SMTP",{
+// //   host: "smtp-mail.outlook.com", //replace with your email provider
+// //   port: 587,
+// //   auth: {
+// //     user: process.env.EMAIL,
+// //     pass: process.env.PASSWORD
+// //   }
+// // });
+
+
+
+// // verify connection configuration
+// transporter.verify(function(error, success) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Server is ready to take our messages");
 //   }
 // });
 
 
 
-// verify connection configuration
-transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+
+// app.post('/send', (req, res, next) => {
+//   var name = req.body.name
+//   var email = req.body.email
+//   var subject = req.body.subject
+//   var message = req.body.message
+
+//   var mail = {
+//     from: name,
+//     to: email,
+//     subject: subject,
+//     text: message
+//   }
+
+//   transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       res.json({
+//         status: 'fail'
+//       })
+//     } else {
+//       res.json({
+//        status: 'success'
+//       })
+//     }
+//   })
+// })
 
 
 
 
-app.post('/send', (req, res, next) => {
-  var name = req.body.name
-  var email = req.body.email
-  var subject = req.body.subject
-  var message = req.body.message
 
-  var mail = {
-    from: name,
-    to: email,
-    subject: subject,
-    text: message
-  }
 
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
+
+// app.use(cors())
+const transporter = nodemailer.createTransport( {
+	// service: "hotmail",
+		host: process.env.MAIL_HOST,
+		port: process.env.MAIL_PORT,
+		// secure: false,
+		auth: {
+			user: process.env.MAIL_USER,
+			pass: process.env.MAIL_PASS
+		}
+		//   tls: {
+		// 	// do not fail on invalid certs
+		// 	rejectUnauthorized: false,
+		//   },
+	})
+
+	transporter.verify(function(error, success) {
+		if (error) {
+		  console.log(error);
+		} else {
+		  console.log("Server is ready to take our messages");
+		}
+	  });
+// app.listen(
+// 	(process.env.PORT || 4000,
+// 	() => {
+// 		console.log("Server is listening on port 4000")
+// 	})
+// )
+
+
+// app.post('/send', (req, res, next) => {
+// 	var name = req.body.name
+// 	var email = req.body.mail
+// 	var subject = req.body.subject
+// 	var message = req.body.message
+// 	// var content = `name: ${name} \n email: ${email} \n subject: ${subject} \n  message: ${message} `
+// 	var mail = {
+// 	  from: name,
+// 	  to: 'meteoben43200@outlook.fr',
+// 	  subject: subject,
+// 	  text: message
+// 	}
+  
+// 	transporter.sendMail(mail, (err, data) => {
+// 	  if (err) {
+// 		res.json({
+// 		  status: 'fail'
+// 		})
+// 	  } else {
+// 		res.json({
+// 		 status: 'success'
+// 		})
+// 	  }
+// 	})
+//   })
+
+
+
+  app.post('/send', (req, res, next) => {
+	var name = req.body.name
+	var email = req.body.email
+	var subject = req.body.subject
+	var message = req.body.message
+	var content = `name: ${name} \n email: ${email} \n subject: ${subject} \n  message: ${message} `
+	var mail = {
+	  from: name,
+	  to: 'meteoben43200@outlook.fr',
+	
+	  text: content
+	}
+  
+	transporter.sendMail(mail, (err, data) => {
+	  if (err) {
+		res.json({
+		  status: 'fail'
+		})
+	  } else {
+		res.json({
+		 status: 'success'
+		})
+	  }
+	})
   })
-})
 
 
 
--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
-import React from "react";
-import axios from "axios";
-// export  { Component }  from "react"
+import axios from "axios"
+import React from "react"
+import "./App.css"
 
 export default class Contact extends React.Component {
 
@@ -13,8 +13,7 @@ export default class Contact extends React.Component {
           message: ''
         }
     }
-
-    onNameChange(event) {
+	onNameChange(event) {
         this.setState({name: event.target.value})
     }
 
@@ -30,11 +29,11 @@ onMsgChange(event) {
         this.setState({message: event.target.value})
     }
 
-    submitEmail(e){
+	submitEmail(e){
         e.preventDefault();
         axios({
           method: "POST", 
-          url:"/send", 
+          url:"http://localhost:5000/send/", 
           data:  this.state
         }).then((response)=>{
           if (response.data.status === 'success'){
@@ -50,52 +49,283 @@ resetForm(){
         this.setState({name: '', email: '',subject:'', message: ''})
 }
 render() {
-    return (
-        <div className="section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="section-title">
-                            <h2 className="title">Contact Us</h2>
-                            <p>Let us know what you think! In order to provide better service,
-                                 please do not hesitate to give us your feedback. Thank you.</p><hr/>
-                            <form id="contact-form" onSubmit={this.submitEmail.bind(this)} 
-                                method="POST">
-                            <div className="form-group">
-                            <div className="row">
-                            <div className="col-md-6">
-                                <input placeholder = "Name"  id="name" type="text" 
-                                   className="form-control" required value={this.state.name} 
-                                   onChange={this.onNameChange.bind(this)}/>
-                            </div>
-                            <div className="col-md-6">
-                                <input placeholder = "Email"  id="email" type="email"
-                                  className="form-control" aria-describedby="emailHelp"
-                                  required value={this.state.email} onChange=
-                                  {this.onEmailChange.bind(this)}/>
-                            </div>
-                            </div>
-                            </div>
-                            <div className="form-group">
-                                <input placeholder = "Subject"  id="subject" type="text"
-                                  className="form-control" required value={this.state.subject}
-                                  onChange={this.onSubjectChange.bind(this)}/>
-                            </div>
-                            <div className="form-group">
-                                <textarea placeholder = "Message"  id="message" 
-                                   className="form-control" rows="1" 
-                                   required value={this.state.message}
-                                   onChange= {this.onMsgChange.bind(this)}/>
-                            </div>
-                            <button type="submit" className="primary-btn submit">Submit</button>
-                            </form>
-                        </div>
-                    </div>
+	return (
+		<div className="section">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<div className="section-title">
+							<h2 className="title">Contact Us</h2>
+							<p>Let us know what you think! In order to provide better service,
+								 please do not hesitate to give us your feedback. Thank you.</p><hr/>
+							<form id="contact-form" onSubmit={this.submitEmail.bind(this)} 
+								method="POST">
+							<div className="form-group">
+							<div className="row">
+							<div className="col-md-6">
+								<input placeholder = "Name"  id="name" type="text" 
+								   className="form-control" required value={this.state.name} 
+								   onChange={this.onNameChange.bind(this)}/>
+							</div>
+							<div className="col-md-6">
+								<input placeholder = "Email"  id="email" type="email"
+								  className="form-control" aria-describedby="emailHelp"
+								  required value={this.state.email} onChange=
+								  {this.onEmailChange.bind(this)}/>
+							</div>
+							</div>
+							</div>
+							<div className="form-group">
+								<input placeholder = "Subject"  id="subject" type="text"
+								  className="form-control" required value={this.state.subject}
+								  onChange={this.onSubjectChange.bind(this)}/>
+							</div>
+							<div className="form-group">
+								<textarea placeholder = "Message"  id="message" 
+								   className="form-control" rows="1" 
+								   required value={this.state.message}
+								   onChange= {this.onMsgChange.bind(this)}/>
+							</div>
+							<button type="submit" className="primary-btn submit">Submit</button>
+							</form>
+						</div>
+					</div>
 
-                </div>
+				</div>
 
-            </div>
-        </div>
-    );
+			</div>
+		</div>
+	);
 }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import axios from "axios";
+// // export  { Component }  from "react"
+
+// export default class Contact extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//           name: '',
+//           email: '',
+//           subject:'',
+//           message: ''
+//         }
+//     }
+
+//     onNameChange(event) {
+//         this.setState({name: event.target.value})
+//     }
+
+// onEmailChange(event) {
+//         this.setState({email: event.target.value})
+//     }
+
+// onSubjectChange(event) {
+//         this.setState({subject: event.target.value})
+//     }
+
+// onMsgChange(event) {
+//         this.setState({message: event.target.value})
+//     }
+
+//     submitEmail(e){
+//         e.preventDefault();
+//         axios({
+//           method: "POST", 
+//           url:"/send", 
+//           data:  this.state
+//         }).then((response)=>{
+//           if (response.data.status === 'success'){
+//               alert("Message Sent."); 
+//               this.resetForm()
+//           }else if(response.data.status === 'fail'){
+//               alert("Message failed to send.")
+//           }
+//         })
+// }
+
+// resetForm(){
+//         this.setState({name: '', email: '',subject:'', message: ''})
+// }
+// render() {
+//     return (
+//         <div className="section">
+//             <div className="container">
+//                 <div className="row">
+//                     <div className="col-md-12">
+//                         <div className="section-title">
+//                             <h2 className="title">Contact Us</h2>
+//                             <p>Let us know what you think! In order to provide better service,
+//                                  please do not hesitate to give us your feedback. Thank you.</p><hr/>
+//                             <form id="contact-form" onSubmit={this.submitEmail.bind(this)} 
+//                                 method="POST">
+//                             <div className="form-group">
+//                             <div className="row">
+//                             <div className="col-md-6">
+//                                 <input placeholder = "Name"  id="name" type="text" 
+//                                    className="form-control" required value={this.state.name} 
+//                                    onChange={this.onNameChange.bind(this)}/>
+//                             </div>
+//                             <div className="col-md-6">
+//                                 <input placeholder = "Email"  id="email" type="email"
+//                                   className="form-control" aria-describedby="emailHelp"
+//                                   required value={this.state.email} onChange=
+//                                   {this.onEmailChange.bind(this)}/>
+//                             </div>
+//                             </div>
+//                             </div>
+//                             <div className="form-group">
+//                                 <input placeholder = "Subject"  id="subject" type="text"
+//                                   className="form-control" required value={this.state.subject}
+//                                   onChange={this.onSubjectChange.bind(this)}/>
+//                             </div>
+//                             <div className="form-group">
+//                                 <textarea placeholder = "Message"  id="message" 
+//                                    className="form-control" rows="1" 
+//                                    required value={this.state.message}
+//                                    onChange= {this.onMsgChange.bind(this)}/>
+//                             </div>
+//                             <button type="submit" className="primary-btn submit">Submit</button>
+//                             </form>
+//                         </div>
+//                     </div>
+
+//                 </div>
+
+//             </div>
+//         </div>
+//     );
+// }
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from "react";
+
+// export default function MyForm() {
+//   const [email, setEmail] = useState("");
+//   const [subject, setSubject] = useState("");
+//   const [message, setMessage] = useState("");
+
+//   const baseUrl = "http://localhost:8000";
+
+//   const sendEmail = async () => {
+//     let dataSend = {
+//       email: email,
+//       subject: subject,
+//       message: message,
+//     };
+
+//     const res = await fetch(`${baseUrl}/email/sendEmail`, {
+//       method: "POST",
+//       body: JSON.stringify(dataSend),
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       // HANDLING ERRORS
+//       .then((res) => {
+//         console.log(res);
+//         if (res.status > 199 && res.status < 300) {
+//           alert("Send Successfully !");
+//         }
+//       });
+//   };
+//   return (
+// <div>
+
+//           <Stack spacing={4}>
+//             <FormControl id="email">
+//               <FormLabel>Email address</FormLabel>
+//               <Input
+//                 type="email"
+//                 placeholder="Receiver's Email Address"
+//                 onChange={(e) => setEmail(e.target.value)}
+//               />
+//             </FormControl>
+//             <FormControl id="email">
+//               <FormLabel>Subject</FormLabel>
+//               <Input
+//                 onChange={(e) => setSubject(e.target.value)}
+//                 type="text"
+//                 placeholder="Enter the subject here..."
+//               />
+//             </FormControl>
+//             <FormControl id="text">
+//               <FormLabel>Message</FormLabel>
+//               <Textarea
+//                 onChange={(e) => setMessage(e.target.value)}
+//                 placeholder="Enter your message here..."
+//               />
+//             </FormControl>
+//             <Stack spacing={10}>
+//               <Button
+//                 bg={"blue.400"}
+//                 color={"white"}
+//                 _hover={{
+//                   bg: "blue.500",
+//                 }}
+//                 onClick={() => sendEmail()}
+//               >
+//                 Send Email
+//               </Button>
+//             </Stack>
+//           </Stack>
+//         </Box>
+//       </Stack>
+//     </Flex>
+  
+//   );
+// }
